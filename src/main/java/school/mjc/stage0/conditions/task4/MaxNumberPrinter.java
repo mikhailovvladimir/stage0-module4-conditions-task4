73911ee811ddf10d94ce11392faeb9ec61347698
CtaRik <1602211@mail.ru>
2024-08-11 04:26:34 +0600

    ok

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        // Find the greatest number among the three
+        int greatest = first;
+
+        if (second > greatest) {
+            greatest = second;
+        }
+
+        if (third > greatest) {
+            greatest = third;
+        }
+        
+        System.out.println(greatest);
     }
 }
